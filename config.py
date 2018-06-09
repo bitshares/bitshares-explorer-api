d2f80f8d82e2928de1895e681a86d97cdaef2c52
Alfredo Garcia <oxarbitrage@gmail.com>
2018-06-09 11:08:00 -0300

    add testnet params to config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 
 WEBSOCKET_URL = os.environ.get('WEBSOCKET_URL', "ws://127.0.0.1:8090/ws")
+WEBSOCKET_PUBLIC_HELPER = os.environ.get('WEBSOCKET_URL', "wss://node.bitshares.eu/ws")
 
 POSTGRES = {'host': os.environ.get('POSTGRES_HOST', 'localhost'),
             'database': os.environ.get('POSTGRES_DATABASE', 'explorer'),
@@ -15,6 +16,8 @@ FULL_WEBSOCKET_URL = os.environ.get('FULL_WEBSOCKET_URL', "ws://88.99.145.10:999
 # a connection to an ElasticSearch wrapper
 ES_WRAPPER = os.environ.get('ES_WRAPPER', "http://185.208.208.184:5000")
 
-
 CORE_ASSET_SYMBOL = 'BTS'
 CORE_ASSET_ID = '1.3.0'
+
+TESTNET = 1 # 0 = not in the testnet, 1 = testnet
+CORE_ASSET_SYMBOL_TESTNET = 'TEST'
