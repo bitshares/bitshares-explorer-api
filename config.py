82d62973201fa02deba839f0ad557e12b6e25da7
Zapata <marco.tessari@gmail.com>
2018-08-18 02:42:01 +0200

    Rollback unwanted change made at 04396001bb5c82c87830b555a2026e3614519ffb.

--- config.py
+++ config.py
@@ -1,18 +1,17 @@
 import os
 
 
-WEBSOCKET_URL = os.environ.get('WEBSOCKET_URL', "ws://localhost:8090/ws")
-#WEBSOCKET_PUBLIC_HELPER = os.environ.get('WEBSOCKET_URL', "wss://node.bitshares.eu/ws")
+WEBSOCKET_URL = os.environ.get('WEBSOCKET_URL', "ws://127.0.0.1:8090/ws")
+WEBSOCKET_PUBLIC_HELPER = os.environ.get('WEBSOCKET_URL', "wss://node.bitshares.eu/ws")
 
 POSTGRES = {'host': os.environ.get('POSTGRES_HOST', 'localhost'),
-            'port': os.environ.get('POSTGRES_PORT', '5432'),
             'database': os.environ.get('POSTGRES_DATABASE', 'explorer'),
             'user': os.environ.get('POSTGRES_USER', 'postgres'),
             'password': os.environ.get('POSTGRES_PASSWORD', 'posta'),
 }
 
 # a connection to a bitshares full node
-FULL_WEBSOCKET_URL = os.environ.get('FULL_WEBSOCKET_URL', "wss://api.fr.bitsharesdex.com")
+FULL_WEBSOCKET_URL = os.environ.get('FULL_WEBSOCKET_URL', "ws://88.99.145.10:9999/ws")
 
 # a connection to an ElasticSearch wrapper
 #ES_WRAPPER = os.environ.get('ES_WRAPPER', "http://185.208.208.184:5000")
@@ -24,5 +23,5 @@ ES_WRAPPER = os.environ.get('ES_WRAPPER', "http://95.216.32.252:5000")
 CORE_ASSET_SYMBOL = 'BTS'
 CORE_ASSET_ID = '1.3.0'
 
-TESTNET = 1 # 0 = not in the testnet, 1 = testnet
+TESTNET = 0 # 0 = not in the testnet, 1 = testnet
 CORE_ASSET_SYMBOL_TESTNET = 'TEST'
