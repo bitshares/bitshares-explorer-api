acc494bcd27aaeb2917083e6bb0fc5e8dabab188
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2019-11-28 11:49:11 +0100

    add response checking to wrapper
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- api/es_wrapper.py
+++ api/es_wrapper.py
@@ -1,19 +1,31 @@
+from .utils import needs_es, verify_es_response
+
 from elasticsearch_dsl import Search, Q
 from services.bitshares_elasticsearch_client import es
-from elasticsearch.exceptions import NotFoundError
 from datetime import datetime, timedelta
 from services.cache import cache
 
-def get_account_history(account_id=None, operation_type=None, from_=0, size=10, 
-                        from_date='2015-10-10', to_date='now', sort_by='-operation_id_num',
-                        search_after=None, type='data', agg_field='operation_type'):
+
+@needs_es()
+def get_account_history(
+        account_id=None,
+        operation_type=None,
+        from_=0,
+        size=10,
+        from_date='2015-10-10',
+        to_date='now',
+        sort_by='-operation_id_num',
+        search_after=None,
+        type='data',  # @ReservedAssignment
+        agg_field='operation_type'
+):
     s = Search(using=es, index="bitshares-*")
     if type == "data":
         s = s.extra(size=size)
         if search_after and search_after != '':
             s = s.extra(search_after=search_after.split(','))
         else:
-            s = s.extra(**{ "from": from_ })
+            s = s.extra(**{"from": from_})
 
     q = Q()
 
@@ -30,23 +42,27 @@ def get_account_history(account_id=None, operation_type=None, from_=0, size=10,
 
     s = s.sort(*sort_by.split(','))
     response = s.execute()
+    verify_es_response(response)
 
     if type == "data":
-        return [ hit.to_dict() for hit in response ]
+        return [hit.to_dict() for hit in response]
     else:
-        return [ field.to_dict() for field in response.aggregations.per_field.buckets ]
+        return [field.to_dict() for field in response.aggregations.per_field.buckets]
 
 
+@needs_es()
 @cache.memoize()
 def get_single_operation(operation_id):
-    s = Search(using=es, index="bitshares-*", extra={"size": 1})
+    s = Search(using=es, index="bitshares-*")
     s.query = Q("match", account_history__operation_id=operation_id)
 
     response = s.execute()
+    verify_es_response(response)
 
-    return [ hit.to_dict() for hit in response ][0]
+    return [hit.to_dict() for hit in response][0]
 
 
+@needs_es()
 def is_alive():
     find_string = datetime.utcnow().strftime("%Y-%m")
     from_date = (datetime.utcnow() - timedelta(days=1)).strftime("%Y-%m-%d")
@@ -61,42 +77,41 @@ def is_alive():
         "head_block_time": None
     }
 
-    try:
-        response = s.execute()
-        if response.aggregations.max_block_time.value is not None:
-            json_response["head_block_time"] = str(response.aggregations.max_block_time.value_as_string)
-            json_response["head_block_timestamp"] = response.aggregations.max_block_time.value
-            json_response["deltatime"] = abs((datetime.utcfromtimestamp(json_response["head_block_timestamp"] / 1000) - json_response["server_time"]).total_seconds())
-            if json_response["deltatime"] < 30:
-                json_response["status"] = "ok"
-            else:
-                json_response["status"] = "out_of_sync"
-                json_response["error"] = "last_block_too_old"
+    response = s.execute()
+    verify_es_response(response)
+
+    if response.aggregations.max_block_time.value is not None:
+        json_response["head_block_time"] = str(response.aggregations.max_block_time.value_as_string)
+        json_response["head_block_timestamp"] = response.aggregations.max_block_time.value
+        json_response["deltatime"] = abs((datetime.utcfromtimestamp(json_response["head_block_timestamp"] / 1000) - json_response["server_time"]).total_seconds())
+        if json_response["deltatime"] < 30:
+            json_response["status"] = "ok"
         else:
             json_response["status"] = "out_of_sync"
-            json_response["deltatime"] = "Infinite"
-            json_response["query_index"] = find_string
-            json_response["query_from_date"] = from_date
-            json_response["error"] = "no_blocks_last_24_hours"
-    except NotFoundError:
+            json_response["error"] = "last_block_too_old"
+    else:
         json_response["status"] = "out_of_sync"
         json_response["deltatime"] = "Infinite"
-        json_response["error"] = "index_not_found"
         json_response["query_index"] = find_string
+        json_response["query_from_date"] = from_date
+        json_response["error"] = "no_blocks_last_24_hours"
 
     return json_response
 
 
+@needs_es()
 @cache.memoize()
 def get_trx(trx, from_=0, size=10):
     s = Search(using=es, index="bitshares-*", extra={"size": size, "from": from_})
     s.query = Q("match", block_data__trx_id=trx)
 
     response = s.execute()
+    verify_es_response(response)
 
-    return [ hit.to_dict() for hit in response ]
+    return [hit.to_dict() for hit in response]
 
 
+@needs_es()
 def get_trade_history(size=10, from_date='2015-10-10', to_date='now', sort_by='-operation_id_num',
                       search_after=None, base="1.3.0", quote="1.3.121"):
 
@@ -118,5 +133,6 @@ def get_trade_history(size=10, from_date='2015-10-10', to_date='now', sort_by='-
 
     s = s.sort(*sort_by.split(','))
     response = s.execute()
+    verify_es_response(response)
 
     return [hit.to_dict() for hit in response]
--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,5 @@
 from functools import wraps
-import elasticsearch
+from elasticsearch.exceptions import NotFoundError
 from flask import abort
 
 
@@ -9,8 +9,7 @@ def index_exists(es, index_name):
 
 
 def needs_es(index_name=None):
-
-    def inner_function(function):
+    def inner_function(function=None):
         @wraps(function)
         def wrapper(*args, **kwargs):
             if index_name is not None:
@@ -18,13 +17,25 @@ def needs_es(index_name=None):
                 pass
             try:
                 return function(*args, **kwargs)
-            except elasticsearch.exceptions.NotFoundError as e:
+            except NotFoundError as e:
                 not_found = getattr(e, "info", {}).get("error", {}).get("root_cause", [{}])[0].get("resource.id", None)
                 message = "The required index does not exist in this ElasticSearch database"
                 if not_found is not None:
                     message = message + " (" + str(not_found) + ")"
-                print(message)
                 abort(404, message)
         return wrapper
 
-    return inner_function
+    if index_name is not None and not isinstance(index_name, str):
+        return inner_function(index_name)
+    else:
+        return inner_function
+
+
+def verify_es_response(response):
+    # if the query took 0 it means no index could be matched!
+    if response.took == 0:
+        raise NotFoundError(404, 'index_not_found_exception', {})
+
+    # if no hits were found, operation_id was invalied
+    if len(response.hits) == 0:
+        abort(404, "Your search did not result in any hits (wrong id?)")
