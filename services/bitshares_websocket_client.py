e5072381cec9f23268a533d62a10e37230007f97
Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>
2020-03-03 13:05:27 +0100

    WebSocket calls are not threadsafe without proper response management
    Signed-off-by: Stefan Schiessl <stefan.schiessl@blockchainprojectsbv.com>

--- services/bitshares_websocket_client.py
+++ services/bitshares_websocket_client.py
@@ -1,36 +1,45 @@
 from websocket import create_connection, WebSocketConnectionClosedException
 import json
 from services.cache import cache
+import threading
+
+import config
+
 
 class RPCError(Exception):
     pass
 
+
 class BitsharesWebsocketClient():
+
+    LOCK = threading.Lock()
+
     def __init__(self, websocket_url):
         self.url = websocket_url
         self._connect()
 
     def _connect(self):
-        self.ws = create_connection(self.url)
+        self.ws = create_connection(self.url, enable_multithread=True)
         self.request_id = 1
         self.api_ids = {
             'database': 0,
             'login': 1
         }
 
-    
     def request(self, api, method_name, params):
-        try:
-            return self._safe_request(api, method_name, params)
-        except WebSocketConnectionClosedException:
-            self.ws.close()
-            self._connect()
-            return self._safe_request(api, method_name, params)
-        except Exception as e:
-            print(e)
-            self.ws.close()
-            self._connect()
-            return self._safe_request(api, method_name, params)
+        # WebSocket calls are not threadsafe without proper response management
+        with BitsharesWebsocketClient.LOCK:
+            try:
+                return self._safe_request(api, method_name, params)
+            except WebSocketConnectionClosedException:
+                self.ws.close()
+                self._connect()
+                return self._safe_request(api, method_name, params)
+            except Exception as e:
+                print(e)
+                self.ws.close()
+                self._connect()
+                return self._safe_request(api, method_name, params)
 
     def _safe_request(self, api, method_name, params):
         api_id = self.load_api_id(api)
@@ -43,12 +52,12 @@ class BitsharesWebsocketClient():
                 params
             ]
         }
-        request_string = json.dumps(payload) 
-        #print('> {}'.format(request_string))
+        request_string = json.dumps(payload)
+        # print('> {}'.format(request_string))
         self.ws.send(request_string)
         self.request_id += 1
-        reply =  self.ws.recv()
-        #print('< {}'.format(reply))
+        reply = self.ws.recv()
+        # print('< {}'.format(reply))
 
         ret = {}
         try:
@@ -56,7 +65,6 @@ class BitsharesWebsocketClient():
         except ValueError:
             raise ValueError("Client returned invalid format. Expected JSON!")
 
-        
         if 'error' in ret:
             if 'detail' in ret['error']:
                 raise RPCError(ret['error']['detail'])
@@ -78,5 +86,5 @@ class BitsharesWebsocketClient():
     def get_global_properties(self):
         return self.request('database', 'get_global_properties', [])
 
-import config
+
 client = BitsharesWebsocketClient(config.WEBSOCKET_URL)
